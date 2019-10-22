9cbf1145382ba9b7019f48c28fb39b95a7999c5d
Steven Tilley <steve@steventilley.com>
2019-10-22 15:08:24 -0400

    add test_cli

---
+++ tests/test_cli.py
@@ -0,0 +1,16 @@
+import subprocess
+from pkg_resources import resource_filename
+from shutil import copyfile
+
+
+def test_cli(tmp_path):
+    t1 = resource_filename('TNT_pipeline_2', f'data/SYS_808.nii.gz')
+    indir = tmp_path / 'in'
+    t1bids = indir / 'sub-1' / 'anat' / 'sub-1_T1w.nii.gz'
+    t1bids.parent.mkdir(parents=True)
+    copyfile(t1, t1bids)
+    outdir = tmp_path / 'out'
+    outdir.mkdir()
+    cmd = ['TNT_pipeline_2', str(indir), str(outdir), 'participant',
+           '--subcortical', '--intracranial_volume', '--skip_validation', '--debug']
+    subprocess.check_call(cmd)
